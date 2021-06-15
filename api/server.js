923afa0052c076cad917cfe7e3e8563044f969b1
Johnny Murillo <jossters@gmail.com>
2021-06-14 22:58:22 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -2,6 +2,32 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+
+const userRouter = require('./users/users-router');
+const authRouter = require('./auth/auth-router');
+
+const sessionConfig = {
+  name: 'chocolatechip',
+  secret: 'safe safe',
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: false,
+  },
+  rolling: true,
+  resave: false,
+  saveUninitialized: false,
+  store: new KnexSessionStore({
+    knex: require('../data/db-config'),
+    tablename: 'session',
+    sidefieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+  }),
+};
+
 /**
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
@@ -17,9 +43,14 @@ const cors = require("cors");
 
 const server = express();
 
+server.use(session(sessionConfig))
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use('/api/auth', authRouter);
+server.use('/api/users', userRouter)
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,5 +1,18 @@
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
+const { restricted } = require('../auth/auth-middleware');
 
+const router = require('express').Router();
+const User = require('./users-model');
+
+router.get('/', restricted, (req, res, next ) => {
+  User.find()
+  .then((user) => {
+    res.json(user)
+  })
+  .catch(next);
+})
+
+module.exports = router;
 
 /**
   [GET] /api/users
